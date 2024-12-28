c314d10675c4644ce710a964b1473052280d3ff9
Aziz-Taufique <taufiqueaziz88@gmail.com>
2024-12-28 19:33:09 +0530

    ternary operator

---
+++ 01_first/03_react/src/Ternery.jsx
@@ -0,0 +1,19 @@
+
+function Turnery() {
+
+    const isGoingOut = true
+
+    // let answer = isGoingOut === true? "yes" : "No"
+
+    // let answer = isGoingOut ? "yes" : "No"
+
+
+  return (
+   <main>
+    <h2></h2>
+    <button>{isGoingOut ? "yes" : "No"}</button>
+   </main>
+  )
+}
+
+export default Turnery
--- 01_first/03_react/src/main.jsx
+++ 01_first/03_react/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 // import Aziz from './Aziz.jsx'
-import Counter from './Counter.jsx'
+// import Counter from './Counter.jsx'
+import Ternery from './Ternery.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <Aziz/> */}
-    <Counter/>
+    {/* <Counter/> */}
+    <Ternery/>
   </StrictMode>,
 )
