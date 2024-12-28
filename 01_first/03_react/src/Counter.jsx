cb2ebc0d2629a2c8260651646c49570a69edd3cd
Aziz-Taufique <taufiqueaziz88@gmail.com>
2024-12-28 19:23:53 +0530

    useState hook

--- 01_first/03_react/src/Counter.jsx
+++ 01_first/03_react/src/Counter.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useState } from "react"
+
 
 function Counter() {
+
+    const [count, setCount] = useState(0)
+    let [val, setVal] = useState(false)
+    console.log(setCount);
+    
+
+    function handleIncrement(){
+        setVal(false)
+        if(count >= 10) {
+            setVal(true)
+            return
+        }
+        setCount(prevCount => prevCount + 1)
+        
+    } 
+
+    function handleDecrement(){
+        if(count <= 0) return
+        setCount(count - 1)
+    }
+
+
   return (
-    <div>Counter</div>
+    <main>
+        <h1>Count is {count}</h1>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+        {val && <h2>count is over</h2>}
+    </main>
   )
 }
 
